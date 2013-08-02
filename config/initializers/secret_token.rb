a24b0880121354200ba03290edfe411759cea7f3
jashmead <john.ashmead@ashmeadsoftware.com>
2013-08-02 18:50:53 -0400

    initial commit for talkvite5

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Talkvite5::Application.config.secret_key_base = 'e1e7979a1401c9f3246137ca0059d851c8d27981e3469221a1c00fee52cc6e4d5cc7a40b280fc26eaeeaabc0c0ba0e774bf552d8719685e39b2eeefac88767ac'
